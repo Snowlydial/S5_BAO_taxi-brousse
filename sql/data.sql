09d3e4c2fcfd96e112f4fbbf54580e99867ec220
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-01-14 10:37:32 +0300

    Forgot the statut for two reservation

--- sql/data.sql
+++ sql/data.sql
@@ -182,7 +182,7 @@ INSERT INTO ReservationStatut (date_annulation, id_reservation) VALUES
 (NULL, 1), (NULL, 2), (NULL, 3), (NULL, 4), (NULL, 5),
 (NULL, 6), (NULL, 7), (NULL, 8), (NULL, 9), (NULL, 10),
 (NULL, 11), (NULL, 12), (NULL, 13), (NULL, 14), (NULL, 15),
-(NULL, 16), (NULL, 17);
+(NULL, 16), (NULL, 17), (NULL, 18), (NULL, 19);
 
 -- Add 2 cancelled reservations
 INSERT INTO ReservationStatut (date_annulation, id_reservation) VALUES 
